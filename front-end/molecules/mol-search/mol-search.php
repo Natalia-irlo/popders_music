20724b53b799f4208c53453d8ab562a1ab256ec0
SharonInfante <sharonshabel@gmail.com>
2023-02-12 18:59:05 +0100

    Created the search missing the functionality with the tables

--- front-end/molecules/mol-search/mol-search.php
+++ front-end/molecules/mol-search/mol-search.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="mol-search.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="content-search">
+        <input type="text" placeholder="Search song" class="search">
+        <img src="../../assets/icons/search.png" class="icon-search"/>
+    </div>
 </body>
 </html>
