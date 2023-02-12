a48e4e654fa435728f89c26b00c1de0a58e5d2d0
SharonInfante <sharonshabel@gmail.com>
2023-02-12 16:55:48 +0100

    home page styles ok, pending drawer, date and title functions

---
+++ front-end/views/home/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="home.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-home">
+        <header>
+            <div class="date">
+                <p class="date-time">Mon, 8:30 <img src="../../assets/icons/sun.png" class="sun"/></p>
+            </div>
+        </header>
+
+    
+        <div class="drawer">
+            <p>AQUI <br>
+            VA <br> 
+            EL <br>
+            MENU
+            </p>
+        </div>
+
+        <div class="home-center">
+            <div class="content-piano">
+                <img src="../../assets/img/home.jpg" alt="Piano" class="piano"/>
+            </div>
+            <div class="content-title">
+                <p class="title-small">-Lunes con L de-</p>
+                <h1 class="title-big">Lograr</h1>
+                <p class="title-small">Todos tus objetivos</p>
+            </div>
+        </div>
+
+        <div class="img-footer">
+            <img src="../../assets/img/austin-neill.jpg" alt="Concert" class="small-picture">
+            <img src="../../assets/img/joshua-fuller.jpg" alt="Girl singing" class="small-picture">
+            <img src="../../assets/img/keagan-henman.jpg" alt="Boy singing" class="small-picture">
+            <img src="../../assets/img/marcela-laskoski.jpg" alt="Dj" class="small-picture">
+        </div>
+    </div>
+
+</body>
+</html>
