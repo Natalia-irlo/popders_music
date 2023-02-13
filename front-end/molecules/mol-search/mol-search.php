bee59d22661948bfe9f1864b0ae1cf5ab8db08f5
Gabriela M. Fernández <galalou2010@gmail.com>
2023-02-13 20:40:53 +0100

    ready search bar and its function

--- front-end/molecules/mol-search/mol-search.php
+++ front-end/molecules/mol-search/mol-search.php
@@ -1,16 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
     <link rel="stylesheet" type="text/css" href="mol-search.css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="content-search">
-        <input type="text" placeholder="Search song" class="search">
-        <img src="../../assets/icons/search.png" class="icon-search"/>
+        <input type="text" placeholder="Escribe un título" class="search" id="search-input">
+        <img src="../../assets/icons/search.png" class="icon-search" id="search-button" />
     </div>
+
+    <div class="container mt-3">
+        <table class="table table-borderless" id="songs-table">
+            <thead>
+                <tr>
+                </tr>
+            </thead>
+            <tbody>
+                <br>
+                <tr>
+                    <br>
+                    <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" />Flowers-Miley Cirus</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Flowers</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Flowers</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Flowers</td>
+                </tr>
+                <tr>
+                    <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" />Animals</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Animals</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Animals</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Animals</td>
+                </tr>
+                <tr>
+                    <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" />Heaven is a Place on Earth</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Heaven is a Place on Earth
+                    </td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Heaven is a Place on Earth
+                    </td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Heaven is a Place on Earth
+                    </td>
+                </tr>
+                <tr>
+                    <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" />Hello</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Hello</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Hello</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Hello</td>
+                </tr>
+                <tr>
+                    <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" />Someone Like You</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Living on a Prayer</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />It's my Life</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Push it</td>
+                </tr>
+                <tr>
+                    <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" />Girls like me</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Don't Stop Believing</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Have a Nice day</td>
+                    <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" />Always</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </div>
+    <script src="./mol-search.js"></script>
 </body>
+
 </html>
