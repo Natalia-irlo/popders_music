4f1cce7154d704acf9c776cb07780956958e1cde
123meena_github <habibimina5@gmail.com>
2023-02-13 14:50:55 +0100

    Tabla

--- front-end/molecules/mol-table/table.php
+++ front-end/molecules/mol-table/table.php
@@ -8,72 +8,88 @@
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="stylesheet" href="../../molecules/mol-date/mol-date-style.css"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200&display=swap"
+        rel="stylesheet" />
 </head>
 
 <body>
-  
+<div class="container-home"> 
     <header>
-      <div class="p-3 text-left bg-light">
-        <div class="form-outline">
-          <i class="fad fa-search"></i>
-          <input type="text" id="form1" class="form-control form-icon-trailing">
-          <label class="form-label" id="form1">Titulo</label>
-          </input>
-        </div>
-      </div>
-    </header>
+      <div class="container"> 
+      <ul class="d-flex">
+                    <li><span id="search-form" class="geolocations col-2">
+                        <a href="#" id="geolocation">    
+                        </a>
+                        </span>
+                      </li>
+                    <li id="date"></li>
+                </ul>
+           
+            <div id="display"></div>
+</header>
 
-    <div class="container mt-3">
-      <table class="table table-borderless">
-        <thead>
-          <tr>
-          </tr>
-        </thead>
-        <tbody>
-          <br>
-          <tr>
-            <br>
-            <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-          </tr>
-          <tr>
-            <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-          </tr>
-          <tr>
-            <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-          </tr>
-          <tr>
-            <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-          </tr>
-          <tr>
-            <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-          </tr>
-          <tr>
-            <td>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-            <td><img className="Dos sm-3" src="./asset/coder.png" height="60px" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+<div class="form-group col-md-4">
+  <input class="form-control rounded-0 py-2" type="search" value="search" id="example-search-input">
+</div>
 
+     <table class="table table-borderless">
+     <thead>
+       <tr>
+       </tr>
+     </thead>
+     <tbody>
+       <br>
+       <tr>
+         <br>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+       </tr>
+       <tr>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+       </tr>
+       <tr>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+       </tr>
+       <tr>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+       </tr>
+       <tr>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+       </tr>
+       <tr>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+         <td><img className="Dos sm-3" src="./../../assets/img/coder.png" height="60px" /></td>
+       </tr>
+     </tbody>
+   </table>
+   </div>
+            </div>
+<script src="../../molecules/mol-date/date.js"></script>
+            
+            
+            
 </body>
 
 </html>
