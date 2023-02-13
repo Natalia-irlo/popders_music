71ced511e2a066aee8ecda154cc72a2a56fd477a
123meena_github <habibimina5@gmail.com>
2023-02-13 11:20:35 +0100

    mol-table

--- front-end/molecules/mol-table/table.php
+++ front-end/molecules/mol-table/table.php
@@ -8,39 +8,13 @@
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
-  <style>
-    .container {
-      background-color: white;
-      opacity: .9;
-      position: absolute;
-      top: 100px;
-      margin-left: 300px;
-      height: 550px;
-      width: 1070px;
-    }
-
-    .p-3 {
-      height: 720px;
-      position: relative;
-      opacity: .5;
-      margin-left: 300px;
-
-    }
-
-    .form-control {
-      width: 300px;
-      height: 30px;
-      margin-top: 40px;
-      margin-left: 100px;
-
-    }
-  </style>
+  
   <div class="bg-image" style="
-    background-image: url('./asset/sean-sinclair-C_NJKfnTR5A-unsplash\.jpg');
+    background-image: url('./asset//sean-sinclair-C_NJKfnTR5A-unsplash.jpg');
     height: 100vh;
   ">
     <header>
