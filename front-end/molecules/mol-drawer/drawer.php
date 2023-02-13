d438c5533758bfe71a4510b8fee4144bb842bda0
natalia <natiriarte5@gmail.com>
2023-02-13 14:42:58 +0100

    drawer finish

--- front-end/molecules/mol-drawer/drawer.php
+++ front-end/molecules/mol-drawer/drawer.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="drawer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Drawer</title>
 </head>
+
 <body>
-    <h1>Drawer</h1>
+    <nav class="nav flex-column" id="drawer">
+        <div class="container-drawer">
+            <a href="" target="_blank" class="nav-link"  aria-current="page">
+              <img class="icon-logo " src="../../../src/logo-popCoder-multicolor.png" width="60px">
+            </a>
+            <a href="" target="_blank" class="nav-link" aria-current="page">
+                <img class="icon" src="../../../src/icono-musica.png" width="70px">
+            </a>
+            <a href="" target="_blank" class="nav-link" aria-current="page">
+                <img class="icon" src="../../../src/icono-gatito.png" width="70px">
+            </a>
+            <a href="" target="_blank" class="nav-link" aria-current="page">
+                <img class="icon" src="../../../src/icono-patito.png" width="70px">
+            </a>
+            <a href="" target="_blank" class="nav-link" aria-current="page">
+                <img class="icon" src="../../../src/icono-user.png" width="70px">
+            </a>
+            
+        </div>
+
+    </nav>
 </body>
+
 </html>
