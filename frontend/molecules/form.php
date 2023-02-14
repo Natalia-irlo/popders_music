32a07a4aa74366ef0a2ac44181ab7daae665bac0
Gabriela M. Fernández <galalou2010@gmail.com>
2023-02-14 18:17:50 +0100

    insert song form logic

--- frontend/molecules/form.php
+++ frontend/molecules/form.php
@@ -1,21 +1,23 @@
 <?php
 
 require_once "../../vendor/autoload.php";
+
 use App\Controllers\Songs;
 
 /*if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }*/
 
-$newCoder = $_POST['coder'];
-$newTitle = $_POST['titulo'];
-$newArtist = $_POST['artist'];
-$newGenre = $_POST['genre'];
-$newURL = $_POST['url'];;
-$newDate = date('Y-m-d H:i:s');
-$newPlayed = false;
+$addIdCoder = filter_input(INPUT_POST, "coder");
+$addTitle = filter_input(INPUT_POST, "titulo");
+$addArtist = filter_input(INPUT_POST, "artist");
+$addGenre = filter_input(INPUT_POST, "genre");
+$addURL = filter_input(INPUT_POST, "url");
+
+$addDate = date('Y-m-d H:i:s');
+$addPlayed = false;
 
 $insertSong = new Songs;
-$insertSong->addRow($newCoder, $newTitle, $newArtist, $newGenre, $newURL, $newDate, $newPlayed);
+$insertSong->addRow($addIdCoder, $addTitle, $addArtist, $addGenre, $addURL, $addDate, $addPlayed);
 
 ?>
 
@@ -31,26 +33,26 @@ $insertSong->addRow($newCoder, $newTitle, $newArtist, $newGenre, $newURL, $newDa
     <form action="form.php" method="post">
         <p>
             <label for="coder">Coder</label>
-            <input id="coder" type="text" name="coder" value="<?= $newCoder['coder'] ?>">
+            <input id="coder" type="text" name="coder">
         </p>
         <p>
             <label for="titulo">Titulo</label>
-            <input id="titulo" type="text" name="titulo" value="<?= $newTitle['titulo'] ?>">
+            <input id="titulo" type="text" name="titulo">
         </p>
         <p>
             <label for="artist">Artist</label>
-            <input id="artist" type="text" name="artist" value="<?= $newArtist['artist'] ?>">
+            <input id="artist" type="text" name="artist">
         </p>
         <p>
             <label for="genre">Genre</label>
-            <input id="genre" type="text" name="genre" value="<?= $newGenre['genre'] ?>">
+            <input id="genre" type="text" name="genre">
         </p>
         <p>
             <label for="url">Url</label>
-            <input id="url" type="text" name="url" value="<?= $newURL['url'] ?>">
+            <input id="url" type="text" name="url">
         </p>
         <p>
-            <input type="submit" value="Guardar">
+            <button>Guardar</button>
         </p>
     </form>
 </body>
